e0f853a38c4e2422e816db26c2db49329b69389e
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-16 12:35:19 -0500

    add responsive js

---
+++ src/components/Navbar/NavbarStyles.jsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import { mediaQueries } from '.././responsiveStyles';
+
+export const Container = styled.div `
+   height: 80px;
+   ${mediaQueries("md")`
+      height: 50px
+   `}
+
+`;
+
+export const NavWrapper = styled.div `
+
+`;
+
+export const NavLeft = styled.div `
+
+`;
+
+export const Logo = styled.div `
+
+`;
+
+export const NavRight = styled.div `
+
+`;
+
+export const MenuItem = styled.div `
+
+`;
